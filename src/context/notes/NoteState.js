799031388f815e651de67ed7e490d34a3baf5942
nikhilkadiyan <kadiyannikhil2@gmail.com>
2024-04-15 10:19:50 +0530

    delete note functionality complete from front and backend

--- src/context/notes/NoteState.js
+++ src/context/notes/NoteState.js
@@ -35,20 +35,21 @@ const NoteState = (props) => {
       body: JSON.stringify({title,description,tag}),
     });
     
-    const note = {
-      "_id": "66152105ndh8850ba0ad4",
-      "user": "661370c3c83facecf2ad1fd3",
-      "title": title,
-      "description": description,
-      "tag": tag,
-      "date": "2024-04-09T11:06:43.713Z",
-      "__v": 0
-    };
-    setNotes(notes.concat(note))
+    const note = await response.json();
+    setNotes(notes.concat(note));
   }
   //Delete a note
-  const deleteNote = (id) => {
-    //To do API call
+  const deleteNote = async(id) => {
+    // API call
+    const response = await fetch(`${host}/api/notes/deletenote/${id}`, {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+        "auth-token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImlkIjoiNjYxMzcwYzNjODNmYWNlY2YyYWQxZmQzIn0sImlhdCI6MTcxMjU1MTQ5OH0.xlLjFJHqfyh-czM3v6sxd5666mExBS6FL6YMOmJz5vI"
+      },
+    });
+    const json = await response.json();
+    console.log(json);
     console.log("Deleting the note with id:" + id);
     const newNotes = notes.filter((note) => { return note._id !== id });
     setNotes(newNotes);
@@ -58,22 +59,21 @@ const NoteState = (props) => {
   const editNote = async (id, title, description, tag) => {
     // API call
     const response = await fetch(`${host}/api/notes/updatenote/${id}`, {
-      method: "POST",
+      method: "PUT",
       headers: {
         "Content-Type": "application/json",
         "auth-token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImlkIjoiNjYxMzcwYzNjODNmYWNlY2YyYWQxZmQzIn0sImlhdCI6MTcxMjU1MTQ5OH0.xlLjFJHqfyh-czM3v6sxd5666mExBS6FL6YMOmJz5vI"
       },
       body: JSON.stringify({title,description,tag}),
     });
-    const json = response.json();
-
+    const updatedNote = await response.json();
+    console.log(updatedNote);
     //Logic to edit in client
     for (let i = 0; i < notes.length; i++) {
-      const element = notes[i];
-      if (element._id === id) {
-        element.title = title;
-        element.description = description;
-        element.tag = tag;
+      if (notes[i]._id === id) {
+        notes[i].title = title;
+        notes[i].description = description;
+        notes[i].tag = tag;
       }
     }
   }
