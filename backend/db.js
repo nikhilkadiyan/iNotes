826db5a85a81e465e44992317fc198c84b5c23c1
nikhilkadiyan <kadiyannikhil2@gmail.com>
2024-04-22 09:26:58 +0530

    fixed bugs in backend

--- backend/db.js
+++ backend/db.js
@@ -1,12 +1,8 @@
 const mongoose = require('mongoose');
 const MONGO_PASS=process.env.MONGO_PASS;
-
+console.log(MONGO_PASS);
 const connectToMongo = ()=>{
-<<<<<<< HEAD
     mongoose.connect(`mongodb+srv://nikhilkadiyan:${MONGO_PASS}@cluster0.hbdkvfp.mongodb.net/inotebook`).then(console.log("connected to mongo successfully"));
-=======
-    mongoose.connect('mongodb+srv://nikhilkadiyan:YOUR_PASSWORD@cluster0.hbdkvfp.mongodb.net/inotebook').then(console.log("connected to mongo successfully"));
->>>>>>> de27008fb0b9c8c4036cbe5c7f7caa692769630f
 }
 
 module.exports = connectToMongo;
