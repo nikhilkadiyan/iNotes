de27008fb0b9c8c4036cbe5c7f7caa692769630f
nikhilkadiyan <127472958+nikhilkadiyan@users.noreply.github.com>
2024-04-22 08:58:48 +0530

    hide mongo password

--- backend/db.js
+++ backend/db.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const connectToMongo = ()=>{
-    mongoose.connect('mongodb+srv://nikhilkadiyan:6OZX6BEUEkzkMIE7@cluster0.hbdkvfp.mongodb.net/inotebook').then(console.log("connected to mongo successfully"));
+    mongoose.connect('mongodb+srv://nikhilkadiyan:YOUR_PASSWORD@cluster0.hbdkvfp.mongodb.net/inotebook').then(console.log("connected to mongo successfully"));
 }
 
 module.exports = connectToMongo;
